d797c7a8faf2e9d7722ea8070fe3e660dc44c6f6
Reinielle Matt <erecereinielle722@gmail.com>
2018-03-02 19:09:53 +0800

    Task class modified

--- SimpleTodo/Task.cs
+++ SimpleTodo/Task.cs
@@ -1,8 +1,20 @@
 ﻿using System;
+using SQLite;
 
 namespace SimpleTodo
 {
     class Task
     {
+        [PrimaryKey, AutoIncrement]
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+
+        public Task() {}
+        public Task(string title, string description)
+        {
+            Title = title;
+            Description = description;
+        }
     }
 }
